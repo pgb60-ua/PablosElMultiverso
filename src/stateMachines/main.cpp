c8b1dee79ad846f06a116447ba9a4b5949054990
Pablo García <pablo.garcia.belando@gmail.com>
2025-10-03 18:12:24 +0200

    Mover archivos stateMachines

--- src/stateMachines/main.cpp
+++ src/stateMachines/main.cpp
@@ -2,6 +2,9 @@
 #include <MainGameState.hpp>
 #include <memory>
 #include <chrono>
+extern "C" {
+    #include <raylib.h>
+}
 
 int main()
 { 
@@ -10,8 +13,13 @@ int main()
     StateMachine state_machine = StateMachine();
     state_machine.add_state(std::make_unique<MainGameState>(), false);
     state_machine.handle_state_changes(delta_time);
+    int screenWidth  = GetMonitorWidth(0);
+    int screenHeight = GetMonitorHeight(0);
+    InitWindow(screenWidth, screenHeight, "Pablos El Multiverso");
+    ToggleFullscreen();
+    InitAudioDevice();
 
-    while (!state_machine.is_game_ending())
+    while (!state_machine.is_game_ending() && IsKeyPressed(KEY_SPACE) == false && !WindowShouldClose())
     {
         state_machine.handle_state_changes(delta_time);
         state_machine.getCurrentState()->handleInput();
