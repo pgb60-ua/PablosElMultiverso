8b7969ac473d0b21e955e425edeb78114d50e33b
Carlos Maria Casado Lopez <carloscasadolopez53@gmail.com>
2025-10-04 11:39:04 +0200

    Abstraido stats a una clase por encima

--- src/interfaces/AEntity.cpp
+++ src/interfaces/AEntity.cpp
@@ -1,24 +1,7 @@
 #include "AEntity.hpp"
 
-AEntity::AEntity(float health, float movementSpeed, float agility,
-                 float attackSpeed, float physicalDamage, float magicDamage,
-                 float armor, float resistance, float criticalChance,
-                 float criticalDamage, float lifeSteal,
-                 float healthRegeneration, std::vector<Texture2D *> textures)
-    : health(health), movementSpeed(movementSpeed), agility(agility),
-      attackSpeed(attackSpeed), physicalDamage(physicalDamage),
-      magicDamage(magicDamage), armor(armor), resistance(resistance),
-      criticalChance(criticalChance), criticalDamage(criticalDamage),
-      lifeSteal(lifeSteal), healthRegeneration(healthRegeneration),
-      attackCooldown(1.0f / attackSpeed), textures(textures) {
-  // La vida como minimo es 1
-  if (this->health < MIN_HEALTH)
-    this->health = MIN_HEALTH;
-  // La regeneracion de vida como minimo es 0
-  if (this->healthRegeneration < MIN_REGENERATION)
-    this->healthRegeneration = MIN_REGENERATION;
-};
+AEntity::AEntity(Stats stats, const Shape &hitbox,
+                 std::vector<Texture2D *> textures)
+    : stats(std::move(stats)), hitbox(hitbox), textures(textures) {}
 
 void AEntity::Render() {}
-
-bool AEntity::IsAlive() {}
--- src/interfaces/AEntity.hpp
+++ src/interfaces/AEntity.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Geometry.hpp"
+#include "Stats.hpp"
 #include <vector>
 extern "C" {
 #include "raylib.h"
@@ -9,10 +10,6 @@ extern "C" {
 // Clase que representa lo mas abstracto de un personaje y enemigo que tienen
 // común
 class AEntity {
-private:
-  static constexpr float MIN_HEALTH = 1.0f;
-  static constexpr float MIN_REGENERATION = 0.0f;
-
 protected:
   /// @brief Vector de texturas de una la entidad
   std::vector<Texture2D *> textures;
@@ -20,142 +17,12 @@ protected:
   /// @brief Hitbox de la entidad (rectangulo - circulo - triangulo)
   Shape hitbox;
 
-  /// @brief Puntos de vida de la entidad
-  float health;
+  /// @brief Variable para almacenar todas las stats de una entidad
+  Stats stats;
 
-  /// @brief Cuantos pixeles se mueve por frame
-  float movementSpeed;
-
-  /// @brief Probabilidad entre 0 - 30 de esquivar un golpe
-  float agility;
-
-  /// @brief Cuantos ataques por segundo se realizan
-  float attackSpeed;
-
-  /// @brief Propiedad autocalculada, tiempo que ha de pasar para volver atacar
-  float attackCooldown;
-
-  /// @brief Puntos de daño de tipo físico que se infligen al atacar
-  float physicalDamage;
-
-  /// @brief Puntos de daño de tipo mágico que se infligen al atacar
-  float magicDamage;
-
-  /// @brief Probabilidad entre 0 - 30 que reduce el daño recibido de tipo
-  /// físico
-  float armor;
-
-  /// @brief Probabilidad entre 0 - 30 que reduce el daño recibido de tipo
-  /// mágico
-  float resistance;
-
-  /// @brief Probabilidad entre 0 - 30 de aplicar más daño físico / mágico
-  float criticalChance;
-
-  /// @brief Multiplicador entre 1 - 10 al daño infligido
-  float criticalDamage;
-
-  /// @brief Porcentaje entre 0 - 50 de curación del daño al atacar
-  float lifeSteal;
-
-  /// @brief Cantidad de puntos de vida que te curas cada segundo
-  float healthRegeneration;
-
-  AEntity(float health, float movementSpeed, float agility, float attackSpeed,
-          float physicalDamage, float magicDamage, float armor,
-          float resistance, float criticalChance, float criticalDamage,
-          float lifeSteal, float healthRegeneration,
-          std::vector<Texture2D *> textures);
+  AEntity(Stats stats, const Shape &hitbox, std::vector<Texture2D *> textures);
 
 public:
-  // Getters de stats
-  /// @brief Obtiene los puntos de vida actuales
-  float GetHealth() const { return health; }
-
-  /// @brief Obtiene la velocidad de movimiento actual
-  float GetMovementSpeed() const { return movementSpeed; }
-
-  /// @brief Obtiene la agilidad actual
-  float GetAgility() const { return agility; }
-
-  /// @brief Obtiene la velocidad de ataque actual
-  float GetAttackSpeed() const { return attackSpeed; }
-
-  /// @brief Obtiene el tiempo de recarga del ataque actual
-  float GetAttackCooldown() const { return attackCooldown; }
-
-  /// @brief Obtiene el daño físico actual
-  float GetPhysicalDamage() const { return physicalDamage; }
-
-  /// @brief Obtiene el daño mágico actual
-  float GetMagicDamage() const { return magicDamage; }
-
-  /// @brief Obtiene la armadura actual
-  float GetArmor() const { return armor; }
-
-  /// @brief Obtiene la resistencia mágica actual
-  float GetResistance() const { return resistance; }
-
-  /// @brief Obtiene la probabilidad de crítico actual
-  float GetCriticalChance() const { return criticalChance; }
-
-  /// @brief Obtiene el multiplicador de daño crítico actual
-  float GetCriticalDamage() const { return criticalDamage; }
-
-  /// @brief Obtiene el porcentaje de robo de vida actual
-  float GetLifeSteal() const { return lifeSteal; }
-
-  /// @brief Obtiene la regeneración de vida por segundo actual
-  float GetHealthRegeneration() const { return healthRegeneration; }
-
-  // Setters de stats
-
-  /// @brief Establece los puntos de vida
-  void SetHealth(float newHealth) { health = newHealth; }
-
-  /// @brief Establece la velocidad de movimiento
-  void SetMovementSpeed(float newSpeed) { movementSpeed = newSpeed; }
-
-  /// @brief Establece la agilidad
-  void SetAgility(float newAgility) { agility = newAgility; }
-
-  /// @brief Establece la velocidad de ataque y recalcula el tiempo de recarga
-  void SetAttackSpeed(float newSpeed) {
-    if (newSpeed <= 0) {
-      attackSpeed = 0.1f;
-    } else
-      attackSpeed = newSpeed;
-    attackCooldown = 1.0f / attackSpeed;
-  }
-
-  /// @brief Establece el daño físico
-  void SetPhysicalDamage(float newDamage) { physicalDamage = newDamage; }
-
-  /// @brief Establece el daño mágico
-  void SetMagicDamage(float newDamage) { magicDamage = newDamage; }
-
-  /// @brief Establece la armadura
-  void SetArmor(float newArmor) { armor = newArmor; }
-
-  /// @brief Establece la resistencia mágica
-  void SetResistance(float newResistance) { resistance = newResistance; }
-
-  /// @brief Establece la probabilidad de crítico
-  void SetCriticalChance(float newChance) { criticalChance = newChance; }
-
-  /// @brief Establece el multiplicador de daño crítico
-  void SetCriticalDamage(float newMultiplier) {
-    criticalDamage = newMultiplier;
-  }
-
-  /// @brief Establece el porcentaje de robo de vida
-  void SetLifeSteal(float newLifeSteal) { lifeSteal = newLifeSteal; }
-
-  /// @brief Establece la regeneración de vida
-  void SetHealthRegeneration(float newRegeneration) {
-    healthRegeneration = newRegeneration;
-  }
-
   /*--------------------------*/
   // Hitbox
   /*--------------------------*/
@@ -184,6 +51,5 @@ public:
   void Render();
   virtual void TakeDamage(float amount) = 0;
   virtual void Update() = 0;
-  virtual bool IsAlive();
   virtual ~AEntity() { textures.clear(); };
 };
