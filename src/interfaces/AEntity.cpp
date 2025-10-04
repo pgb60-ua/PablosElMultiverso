88ce0be3cde355035c50152a1f76ff16fdf8cec9
Carlos Maria Casado Lopez <carloscasadolopez53@gmail.com>
2025-10-04 12:28:21 +0200

    añadida logica de comprobacion de minimos de stats en entidad y añadido lo relacionado con si puede atacar

--- src/interfaces/AEntity.cpp
+++ src/interfaces/AEntity.cpp
@@ -1,7 +1,24 @@
 #include "AEntity.hpp"
 
-AEntity::AEntity(Stats stats, const Shape &hitbox,
+AEntity::AEntity(Stats stats, bool canShoot, const Shape &hitbox,
                  std::vector<Texture2D *> textures)
-    : stats(std::move(stats)), hitbox(hitbox), textures(textures) {}
+    : stats(std::move(stats)), canShoot(canShoot), hitbox(hitbox),
+      textures(textures) {
+  // No se puede crear a una entidad con 0 de vida
+  if (this->stats.GetHealth() <= 0)
+    this->stats.SetHealth(MIN_HEALTH);
+  // No se puede crear a una entidad con regeneracion negativa
+  if (this->stats.GetHealthRegeneration() < 0)
+    this->stats.SetHealthRegeneration(MIN_REGENERATION);
+  // Si la entidad puede disparar calculamos su cooldown
+  if (canShoot)
+    attackCooldown = 1.0f / stats.GetAttackSpeed();
+  else
+    attackCooldown = 0;
+}
 
 void AEntity::Render() {}
+
+bool AEntity::IsAlive() { return stats.GetHealth() > 0; }
+
+bool AEntity::CanAttack() { return stats.GetAttackSpeed() > 0; }
--- src/interfaces/AEntity.hpp
+++ src/interfaces/AEntity.hpp
@@ -10,9 +10,16 @@ extern "C" {
 // Clase que representa lo mas abstracto de un personaje y enemigo que tienen
 // común
 class AEntity {
+private:
+  static constexpr float MIN_HEALTH = 1.0f;
+  static constexpr float MIN_REGENERATION = 0.0f;
+
 protected:
   /// @brief Vector de texturas de una la entidad
   std::vector<Texture2D *> textures;
+
+  /// @brief La entidad puede disparar?
+  bool canShoot;
 
   /// @brief Hitbox de la entidad (rectangulo - circulo - triangulo)
   Shape hitbox;
@@ -20,9 +27,15 @@ protected:
   /// @brief Variable para almacenar todas las stats de una entidad
   Stats stats;
 
-  AEntity(Stats stats, const Shape &hitbox, std::vector<Texture2D *> textures);
+  /// @brief Propiedad autocalculada, tiempo que ha de pasar para volver atacar
+  float attackCooldown;
+
+  AEntity(Stats stats, bool canShoot, const Shape &hitbox,
+          std::vector<Texture2D *> textures);
 
 public:
+  /// @brief Obtiene el tiempo de recarga del ataque actual
+  float GetAttackCooldown() const { return attackCooldown; }
   /*--------------------------*/
   // Hitbox
   /*--------------------------*/
@@ -49,6 +62,10 @@ public:
   };
 
   void Render();
+  /// @brief Devuelve si la entidad tiene mas de 0 de vida
+  bool IsAlive();
+  /// @brief Devuelve si la entidad tiene mas de 0 de velocidad de ataque
+  bool CanAttack();
   virtual void TakeDamage(float amount) = 0;
   virtual void Update() = 0;
   virtual ~AEntity() { textures.clear(); };
