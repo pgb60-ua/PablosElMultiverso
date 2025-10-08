506e597295eed210631a44141ba608b66d1f8d7d
Manuel José Tornero Fuster <manueljosetornerofuster@gmail.com>
2025-10-08 16:17:56 +0200

    Base para clase de proyectiles

---
+++ include/AProjectile.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "Stats.hpp"
+
+class AProjectile
+{
+private:
+    Stats stats;
+
+public:
+    AProjectile(/* args */);
+    ~AProjectile();
+};
+
+AProjectile::AProjectile(/* args */)
+{
+}
+
+AProjectile::~AProjectile()
+{
+}
