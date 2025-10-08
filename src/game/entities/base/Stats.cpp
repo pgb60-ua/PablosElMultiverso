97a305340fa7d6de2cabaa39e7156ce3f3817c50
Carlos Maria Casado Lopez <carloscasadolopez53@gmail.com>
2025-10-08 16:05:11 +0200

    cambiado modo de importar

--- src/game/entities/base/Stats.cpp
+++ src/game/entities/base/Stats.cpp
@@ -1,4 +1,4 @@
-#include "Stats.hpp"
+#include <Stats.hpp>
 
 Stats::Stats(const OffensiveStats& offensiveStats, const DefensiveStats& defensiveStats)
 : offensiveStats(offensiveStats),defensiveStats(defensiveStats){};
