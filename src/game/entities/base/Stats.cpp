09391c6a6d315cd09c1631750a9a327fc06c7f37
Manuel José Tornero Fuster <91553219+mjtf3@users.noreply.github.com>
2025-10-08 20:07:59 +0200

    PEM-66 | Interfaz de municion (#10)
    * Base para clase de proyectiles
    
    * Implementación básica de la clase proejectil
    
    * Mejora en la implementación del proyectil base y mejora de la localización de dicha implementación (.cpp)
    
    * Fix bug constructor con constantes
    
    * Revertimos cambios, culpa de carlos
    
    * Arreglo errores menores
    
    * ahora si
    
    * ya

--- src/game/entities/base/Stats.cpp
+++ src/game/entities/base/Stats.cpp
@@ -1,4 +1,4 @@
 #include <Stats.hpp>
 
-Stats::Stats(const OffensiveStats& offensiveStats, const DefensiveStats& defensiveStats)
-: offensiveStats(offensiveStats),defensiveStats(defensiveStats){};
+Stats::Stats(const OffensiveStats &offensiveStats, const DefensiveStats &defensiveStats)
+    : offensiveStats(offensiveStats), defensiveStats(defensiveStats) {};
