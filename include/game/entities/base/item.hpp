8edcf7f171b7efd12a330da248b8b428dab201f8
Carlos Maria Casado Lopez <carloscasadolopez53@gmail.com>
2025-10-11 13:07:53 +0200

    Added variable para guardar el sprite del item

--- include/game/entities/base/item.hpp
+++ include/game/entities/base/item.hpp
@@ -1,8 +1,9 @@
 #pragma once
+#include "stats.hpp"
 #include <string>
-#include <stats.hpp>
-extern "C" {
-    #include <raylib.h>
+extern "C"
+{
+#include <raylib.h>
 }
 
 enum class ItemRarity
@@ -13,46 +14,49 @@ enum class ItemRarity
     Epic
 };
 
-class Item {
-    protected:
-        /// @brief nombre del objeto
-        std::string name;
-        /// @brief  Descripción del objeto
-        std::string description;
-        /// @brief Estadisticas del objeto
-        Stats stats;
-        /// @brief Rareza del objeto
-        ItemRarity itemRarity;
+class Item
+{
+protected:
+    /// @brief nombre del objeto
+    std::string name;
+    /// @brief  Descripción del objeto
+    std::string description;
+    /// @brief Estadisticas del objeto
+    Stats stats;
+    /// @brief Rareza del objeto
+    ItemRarity itemRarity;
+    /// @brief Textura del item
+    Texture2D sprite;
 
-    public:
-        /// @brief Constructor de la clase Item
+public:
+    /// @brief Constructor de la clase Item
 
-        Item(std::string name, std::string description, Stats stats, ItemRarity itemRarity);
+    Item(std::string name, std::string description, Stats stats, ItemRarity itemRarity, Texture2D sprite);
 
-        /// @brief Getter del nombre del objeto
-        std::string GetName() const { return name; }
+    /// @brief Getter del nombre del objeto
+    std::string GetName() const { return name; }
 
-        /// @brief Getter de la descripcion del objeto
-        std::string GetDescription() const { return description; }
+    /// @brief Getter de la descripcion del objeto
+    std::string GetDescription() const { return description; }
 
-        /// @brief Getter de las estadisticas del objeto
-        Stats GetStats() const { return stats; }
+    /// @brief Getter de las estadisticas del objeto
+    Stats GetStats() const { return stats; }
 
-        /// @brief Getter de la rareza del objeto
-        ItemRarity GetItemRarity() const { return itemRarity; }
+    /// @brief Getter de la rareza del objeto
+    ItemRarity GetItemRarity() const { return itemRarity; }
 
-        // Setters
-        /// @brief Setter del nombre del objeto
-        void SetName(const std::string& newName) { name = newName; }
+    // Setters
+    /// @brief Setter del nombre del objeto
+    void SetName(const std::string &newName) { name = newName; }
 
-        /// @brief Setter de la descripcion del objeto
-        void SetDescription(const std::string& newDescription) { description = newDescription; }
+    /// @brief Setter de la descripcion del objeto
+    void SetDescription(const std::string &newDescription) { description = newDescription; }
 
-        /// @brief Setter de las estadisticas del objeto
-        void SetStats(const Stats& newStats) { stats = newStats; }
+    /// @brief Setter de las estadisticas del objeto
+    void SetStats(const Stats &newStats) { stats = newStats; }
 
-        /// @brief Setter de la rareza del objeto
-        void SetItemRarity(ItemRarity newRarity) { itemRarity = newRarity; }
+    /// @brief Setter de la rareza del objeto
+    void SetItemRarity(ItemRarity newRarity) { itemRarity = newRarity; }
 
-        virtual ~Item() {}
+    virtual ~Item() {}
 };
--- src/game/entities/base/item.cpp
+++ src/game/entities/base/item.cpp
@@ -1,4 +1,6 @@
-#include <item.hpp>
+#include "Item.hpp"
 
-Item::Item(std::string name, std::string description, Stats stats, ItemRarity itemRarity)
-    : name(name), description(description), stats(stats), itemRarity(itemRarity) {}
+Item::Item(std::string name, std::string description, Stats stats, ItemRarity itemRarity, Texture2D sprite)
+    : name(name), description(description), stats(stats), itemRarity(itemRarity), sprite(sprite)
+{
+}
