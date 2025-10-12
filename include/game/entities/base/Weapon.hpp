10f39b2e2d5e40463f2e648824840e5b4e2618a0
Alejandro <alejandrojimenez200420@gmail.com>
2025-10-12 16:34:19 +0200

    Añadida clase Weapon

---
+++ include/game/entities/base/Weapon.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <Item.hpp>
+#include <string>
+
+
+enum class WeaponType
+{
+    Melee,
+    Ranged
+};
+
+class Weapon : public Item {
+protected:
+    /// @brief Tipo de arma
+    WeaponType weaponType;
+    /// @brief Nivel del arma
+    int level;
+    /// @brief Nivel máximo del arma
+    const int maxLevel = 4;
+
+public:
+    /// @brief Constructor de la clase Weapon
+    Weapon(const std::string name, const std::string description, const Stats stats, ItemRarity itemRarity, WeaponType weaponType, int level);
+    /// @brief Getter del tipo de arma
+    WeaponType GetWeaponType() const { return weaponType; }
+    /// @brief Getter del nivel del arma
+    int GetLevel() const { return level; }
+    /// @brief Getter del nivel máximo del arma
+    int GetMaxLevel() const { return maxLevel; }
+    /// @brief Sube de nivel el arma
+    virtual bool Upgrade(const OffensiveStats& newOffensiveStats);
+
+
+    /// @brief Método para atacar
+    virtual void Attack() = 0;
+    virtual ~Weapon() override = default;
+
+};
---
+++ src/game/entities/base/Weapon.cpp
@@ -0,0 +1,26 @@
+#include <Weapon.hpp>
+
+Weapon::Weapon(std::string name, std::string description, Stats stats, ItemRarity itemRarity, WeaponType weaponType, int level)
+    : Item(name, description, stats, itemRarity), weaponType(weaponType), level(level) {}
+
+
+bool Weapon::Upgrade(const OffensiveStats& newOffensiveStats) {
+    if (level >= maxLevel) {
+        return false;
+    }
+    level++;
+
+    OffensiveStats currentStats = stats.GetOffensiveStats();
+    OffensiveStats upgradedStats = {
+        currentStats.physicalDamage + newOffensiveStats.physicalDamage,
+        currentStats.magicDamage + newOffensiveStats.magicDamage,
+        currentStats.attackSpeed + newOffensiveStats.attackSpeed,
+        std::min(currentStats.criticalChance + newOffensiveStats.criticalChance, 30.0f),
+        std::min(currentStats.criticalDamage + newOffensiveStats.criticalDamage, 10.0f),
+        std::min(currentStats.lifeSteal + newOffensiveStats.lifeSteal, 50.0f)
+    };
+
+    stats.SetOffensiveStats(upgradedStats);
+
+    return true;
+}
