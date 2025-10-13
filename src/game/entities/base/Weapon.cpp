f30bbbfc251eec2018458a6f6ff6f3d5e56c7c2d
Alejandro <alejandrojimenez200420@gmail.com>
2025-10-13 18:01:59 +0200

    Añadida constante para los numeros magicos

--- src/game/entities/base/Weapon.cpp
+++ src/game/entities/base/Weapon.cpp
@@ -1,5 +1,9 @@
 #include <Weapon.hpp>
 
+const float MAX_CRITICAL_CHANCE = 30.0f;
+const float MAX_CRITICAL_DAMAGE = 10.0f;
+const float MAX_LIFE_STEAL = 50.0f;
+
 Weapon::Weapon(const std::string& name, const std::string& description, const Stats& stats, ItemRarity itemRarity, WeaponType weaponType, int level)
     : Item(name, description, stats, itemRarity), weaponType(weaponType), level(level) {}
 
@@ -15,9 +19,9 @@ bool Weapon::Upgrade(const OffensiveStats& newOffensiveStats) {
         currentStats.physicalDamage + newOffensiveStats.physicalDamage,
         currentStats.magicDamage + newOffensiveStats.magicDamage,
         currentStats.attackSpeed + newOffensiveStats.attackSpeed,
-        std::min(currentStats.criticalChance + newOffensiveStats.criticalChance, 30.0f),
-        std::min(currentStats.criticalDamage + newOffensiveStats.criticalDamage, 10.0f),
-        std::min(currentStats.lifeSteal + newOffensiveStats.lifeSteal, 50.0f)
+        std::min(currentStats.criticalChance + newOffensiveStats.criticalChance, MAX_CRITICAL_CHANCE),
+        std::min(currentStats.criticalDamage + newOffensiveStats.criticalDamage, MAX_CRITICAL_DAMAGE),
+        std::min(currentStats.lifeSteal + newOffensiveStats.lifeSteal, MAX_LIFE_STEAL)
     };
 
     stats.SetOffensiveStats(upgradedStats);
