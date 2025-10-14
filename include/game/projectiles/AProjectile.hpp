d2e0034bde5117b5898ac4b440c5f77e21fa9c65
Manuel Tornero <manueljosetornerofuster@gmail.com>
2025-10-14 22:47:58 +0200

    pana

--- include/game/projectiles/AProjectile.hpp
+++ include/game/projectiles/AProjectile.hpp
@@ -11,20 +11,24 @@ private:
     Stats stats;
     Vector2 position;
     Vector2 direction;
+    bool active;
+    float speed;
 
 public:
-    AProjectile() = default;
-    ~AProjectile() = default;
+    AProjectile();
+    virtual ~AProjectile();
 
     /// @brief Actualiza la posición del proyectil
     /// @details Mueve el proyectil en la dirección especificada a una velocidad determinada por las estadísticas del proyectil.
-    /// @note Para comprobar si se tiene que mover el proyectil, se debe comprobar si está activo(tiene una posicion destino válida).
+    /// @note Para comprobar si se tiene que mover el proyectil, se debe comprobar si está activo.
     /// @param deltaTime
-    virtual void update(float deltaTime) = 0;
+    virtual void update(float deltaTime);
 
-    Vector2 getPosition() const;
-    Stats getStats() const;
+    Vector2 getPosition() const { return position; }
+    Vector2 getDirection() const { return direction; }
+    Stats getStats() const { return stats; }
 
     void deactivate();
+    bool isActive() const { return active; }
     void activate(Vector2 position, Vector2 direction, const Stats &stats);
 };
