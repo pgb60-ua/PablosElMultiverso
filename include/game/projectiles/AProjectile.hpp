7121ef8eb90e5717694d9fe7a1c94ede7031f66a
Manuel Tornero <manueljosetornerofuster@gmail.com>
2025-10-14 22:24:23 +0200

    eliminación booleano

--- include/game/projectiles/AProjectile.hpp
+++ include/game/projectiles/AProjectile.hpp
@@ -11,16 +11,20 @@ private:
     Stats stats;
     Vector2 position;
     Vector2 direction;
-    bool active = false;
 
 public:
     AProjectile() = default;
     ~AProjectile() = default;
 
+    /// @brief Actualiza la posición del proyectil
+    /// @details Mueve el proyectil en la dirección especificada a una velocidad determinada por las estadísticas del proyectil.
+    /// @note Para comprobar si se tiene que mover el proyectil, se debe comprobar si está activo(tiene una posicion destino válida).
+    /// @param deltaTime
     virtual void update(float deltaTime) = 0;
+
     Vector2 getPosition() const;
     Stats getStats() const;
-    bool isActive() const;
+
     void deactivate();
     void activate(Vector2 position, Vector2 direction, const Stats &stats);
 };
