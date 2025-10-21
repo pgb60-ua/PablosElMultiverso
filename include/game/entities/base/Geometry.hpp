374d6a6931f39b18c1bf7586f75f11ae77a97c0b
Pablo García <pablo.garcia.belando@gmail.com>
2025-10-21 16:48:24 +0200

    makefile

--- include/game/entities/base/Geometry.hpp
+++ include/game/entities/base/Geometry.hpp
@@ -33,33 +33,6 @@ typedef struct
     ShapeData data;
 } Shape;
 
-Vector2 getShapePosition(const Shape &shape)
-{
-    switch (shape.type)
-    {
-    case SHAPE_CIRCLE:
-        return shape.data.circle.center;
-    case SHAPE_RECTANGLE:
-        return {shape.data.rectangle.x, shape.data.rectangle.y};
-    default:
-        break;
-    }
+Vector2 getShapePosition(const Shape &shape);
 
-    return {-1, -1};
-}
-
-void setShapePosition(Shape &shape, const Vector2 &position)
-{
-    switch (shape.type)
-    {
-    case SHAPE_CIRCLE:
-        shape.data.circle.center = position;
-        break;
-    case SHAPE_RECTANGLE:
-        shape.data.rectangle.x = position.x;
-        shape.data.rectangle.y = position.y;
-        break;
-    default:
-        break;
-    }
-}
+void setShapePosition(Shape &shape, const Vector2 &position);
