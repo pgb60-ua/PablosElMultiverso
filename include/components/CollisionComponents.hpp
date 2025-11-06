cec554dcc006a1d5b862708b5e30f9b1326d0bdd
Carlos Maria Casado Lopez <carloscasadolopez53@gmail.com>
2025-11-06 19:29:43 +0100

    Added CollisionComponents

---
+++ include/components/CollisionComponents.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+typedef struct
+{
+    float width;
+    float height;
+    float OffsetX;
+    float OffsetY;
+} RectangleHitboxComponent;
+
+typedef struct
+{
+    float radius;
+    float offsetX;
+    float offsetY;
+} CircleHitboxComponent;
