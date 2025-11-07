573c277645273a4c2e9d8ecee35009d5e7512f61
Alejandro Jiménez Martínez <123954076+ajm68-ua@users.noreply.github.com>
2025-11-07 23:20:50 +0100

    PEM-88 | Crear clase SniperWeapon (#49)
    * Creado .hpp de SniperWeapon
    
    * Creado .cpp de SniperWeapon
    
    * Añadidas clases de Sniper (sin probar por falta de proyectil)
    
    * Cambiado a SniperProjectile
    
    * Añadido método para agregar estadisticas del player al arma
    
    * Quitada redundancia
    
    * Añadido el @brief
    
    * Resueltos números mágicos
    
    * Añadidos mínimos para impedir exceder valores
    
    * mayusculas mimimimi
    
    * Revert "mayusculas mimimimi"
    
    This reverts commit d85e37dfe36c94db1b3fd448334f8a0ae1883b0d.
    
    * Ahora si, resuelto

---
+++ src/game/entities/base/SniperWeapon.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include "SniperWeapon.hpp"
+
+// Helper lambdas para obtener datos del JSON
+
+
+SniperWeapon::SniperWeapon(const Vector2& position, std::vector<AEnemy*>& enemiesInRange)
+    : ARangeWeapon(
+        GetStringFromJSON("name", WEAPON_TYPE::SNIPER, "Unknown Weapon"),
+        GetStringFromJSON("description", WEAPON_TYPE::SNIPER, ""),
+        DataFileManager::GetInstance().GetWeaponStats(WEAPON_TYPE::SNIPER),
+        GetRarityFromJSON(WEAPON_TYPE::SNIPER),
+        GetIntFromJSON("level", WEAPON_TYPE::SNIPER, 1),
+        GetIntFromJSON("pool_size", WEAPON_TYPE::SNIPER, 50),
+        position,
+        enemiesInRange
+    )
+{
+    InitializeProjectilePool();
+    SetPosition(position);
+    SetWeaponType(WEAPON_TYPE::SNIPER);
+}
+
+SniperWeapon::~SniperWeapon() {
+}
+
+void SniperWeapon::Attack(const Vector2& position, float deltaTime) {
+    timeSinceLastAttack += deltaTime;
+    if (timeSinceLastAttack >= attackInterval) {
+        ShootProjectile(position, direction);
+        timeSinceLastAttack -= attackInterval;
+    }
+}
+
+std::unique_ptr<AProjectile> SniperWeapon::CreateProjectile() {
+    return std::make_unique<SniperProjectile>();
+}
