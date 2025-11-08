00632f529348cfe022329a672cc86de0a5e850d3
Alejandro <alejandrojimenez200420@gmail.com>
2025-11-08 10:18:35 +0100

    Corregidos errores de compilación de Sniper

--- include/game/entities/base/SniperWeapon.hpp
+++ include/game/entities/base/SniperWeapon.hpp
@@ -7,7 +7,7 @@
 class SniperWeapon : public ARangeWeapon {
 public:
     /// @brief Constructor de la clase SniperWeapon que carga desde JSON
-    SniperWeapon(const Vector2& position, std::vector<AEnemy*>& enemiesInRange);
+    SniperWeapon(const Vector2& position, std::vector<AEnemy*>& enemiesInRange, std::vector<AEnemy *> &allEnemies);
     virtual ~SniperWeapon();  
 
     /// @brief Método para atacar
