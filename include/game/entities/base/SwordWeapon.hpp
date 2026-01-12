f4bd831a14f6092d79c85959a59ce9bc1cabdd91
Alejandro Jiménez Martínez <123954076+ajm68-ua@users.noreply.github.com>
2026-01-12 11:10:20 +0100

    PEM-151 | Creada SwordWeapon (#115)
    * Agregada orbita a las armas
    
    * Agregada hacha y su forma de ataque
    
    * Cambios para que las armas orbiten
    
    * Arreglos en calculos de dirección
    
    * Modificada rotación y angulo para que sea de -120 a 120º
    
    * más validaciones de enemigos
    
    * Eliminadas funciones de Offset que ya no se usaban
    
    * Arreglos en el intervalo de ataque y constantes
    
    * Modificada constante y añadida comprobación de pegar al borde del mapa en el huevo
    
    * Constante
    
    * Modificación de Axe
    
    * Creada SwordWeapon y json con sus datos
    
    * Errores de conflictos

---
+++ include/game/entities/base/SwordWeapon.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "AMeleeWeapon.hpp"
+#include "DataFileManager.hpp"
+#include <vector>
+#include <memory>
+
+class SwordWeapon : public AMeleeWeapon {
+private:
+    bool isThrusting = false;
+    float thrustTimer = 0.0f;
+    float currentThrustAmount = 0.0f;
+    
+    static constexpr float THRUST_DURATION = 0.3f;
+    static constexpr float THRUST_DISTANCE = 40.0f;
+    
+    static constexpr float DEFAULT_ATTACK_SIZE = 30.0f;
+    std::vector<AEnemy*> hitEnemies;
+
+public:
+    SwordWeapon(const Vector2& position, std::vector<AEnemy *>& enemiesInRange, std::vector<AEnemy *> &allEnemies);
+    virtual ~SwordWeapon();
+    
+    void Attack() override;
+    void Attack(float deltaTime) override;
+    void update(float deltaTime, const Vector2 &position) override;
+    //void render() override;
+};
