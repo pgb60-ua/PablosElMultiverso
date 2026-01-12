fa880c30e75adb855fb70d1833908c20b51238f3
Manuel Tornero <manueljosetornerofuster@gmail.com>
2026-01-12 17:16:04 +0100

    tiene razon

--- src/game/entities/player/Player.cpp
+++ src/game/entities/player/Player.cpp
@@ -63,11 +63,11 @@ void Player::Move(Vector2 newDirection, float deltaTime)
     Vector2 position = GetPosition();
     Vector2 newPosition =
         Vector2{position.x + newDirection.x * deltaTime * speed, position.y + newDirection.y * deltaTime * speed};
-    if (newPosition.x > GetScreenWidth())
+    if (newPosition.x + hitbox.data.rectangle.width > GetScreenWidth())
     {
         newPosition.x = GetScreenWidth() - hitbox.data.rectangle.width;
     }
-    if (newPosition.y > GetScreenHeight())
+    if (newPosition.y + hitbox.data.rectangle.height > GetScreenHeight())
     {
         newPosition.y = GetScreenHeight() - hitbox.data.rectangle.height;
     }
